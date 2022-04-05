888b864acccdb92b29e21063b4175ca6037dc4db
Matthew Silverman <matthews@xanadu.ai>
2022-04-05 23:48:05 +0000

    install.sh works for linux or darwin

--- install.sh
+++ install.sh
@@ -12,19 +12,25 @@ if [[ -e ~/.zsh_aliases ]]; then
 fi
 
 # install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+echo "Installing oh-my-zsh"
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # install brew
-sudo xcode-select --install
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zshrc
-echo "Added brew environment setup to ~/.zshrc"
+if [[ $(uname -s) == Darwin ]]; then
+    echo "Installing brew"
+    sudo xcode-select --install
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zshrc
+    echo "Added brew environment setup to ~/.zshrc"
+fi
 
 # install nerdtree vim plugin
+echo "Installing vim plugins"
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone https://github.com/preservim/nerdtree.git ~/.vim/bundle/nerdtree
 
 # install all ~/.*rc files to configure env, add aliases to .zshrc
+echo "Installing dotfiles to home directory"
 PWD=$(pwd)
 mv $PWD/.git $PWD/git
 cp $PWD/.* ~/
@@ -32,7 +38,6 @@ echo "source ~/.zsh_aliases" >> ~/.zshrc
 cat >> ~/.zshrc << EOM
 if [[ -z $WAS_SOURCED ]]; then
     source ~/.zsh_aliases
-    eval "$(/opt/homebrew/bin/brew shellenv)"
     WAS_SOURCED=1
 fi
 EOM
